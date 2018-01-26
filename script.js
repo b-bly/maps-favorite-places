df3b49b8414a376df0564ef5933ffc4b341fcfdb
Brendt Bly <brendt.bly@outlook.com>
2018-01-26 14:07:01 -0600

    ngMaps added

--- script.js
+++ script.js
@@ -2,10 +2,21 @@ console.log('js');
 
 var app = angular.module('myApp', ['ngMap']);
 
-app.controller('myController', ['$http', 'NgMap', function($http, NgMap) {
+app.controller('myController', ['$http', 'NgMap', function ($http, NgMap) {
     console.log(' Controller has been loaded');
     var self = this;
     var key = config.MAPS_KEY;
+    var myLatLng = { lat: -25.363, lng: 131.044 }
+    self.bounds = new google.maps.LatLngBounds();
+    self.map;
+    self.marker = {
+        position: myLatLng,
+        setMap: map,
+        title: 'Hello World!'
+    };
+
+
+
     function initMap() {
         var uluru = { lat: -25.363, lng: 131.044 };
         // var map = new google.maps.Map(document.getElementById('map'), {
@@ -16,12 +27,19 @@ app.controller('myController', ['$http', 'NgMap', function($http, NgMap) {
         //     position: uluru,
         //     map: map
         // });
-        NgMap.getMap().then(function(map) {
-            console.log(map.getCenter());
-            console.log('markers', map.markers);
-            console.log('shapes', map.shapes);
-          });
+
     }
 
-    self.googleMapsUrl="https://maps.googleapis.com/maps/api/js?key=" + key;
+    var latlng = new google.maps.LatLng(myLatLng.lat, myLatLng.lng)
+    self.bounds.extend(latlng);
+
+    self.googleMapsUrl = "https://maps.googleapis.com/maps/api/js?key=" + key;
+
+    NgMap.getMap().then(function (map) {
+        console.log(map.getCenter());
+        console.log('markers', map.markers);
+        console.log('shapes', map.shapes);
+        self.map = map;
+        //self.map.fitBounds(self.bounds);
+    });
 }]);
